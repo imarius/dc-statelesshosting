69977f598467f6068d6d4c10a8e6c047e6248e5d
MESHDIGITAL\alice.burdujanu <alice.burdujanu@heg.com>
2017-03-26 13:44:36 +0300

    TXT record check

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using DnsClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using StatelessHosting.Models;
 
 namespace StatelessHosting.Controllers
 {
     public class HomeController : Controller
     {
+        private IDnsQueryResponse DNSQueryResponse { get; set; }
         public IActionResult Index()
         {
             string method = HttpContext.Request.Method;
+
+            HostString host = HttpContext.Request.Host;
+            string hostname = host.Host;
+            var lookup = new LookupClient(IPAddress.Parse("8.8.8.8"));
+            string txtName = string.Empty;
+
+            DNSQueryResponse = lookup.Query(hostname, QueryType.TXT);
+            foreach (var _answer in DNSQueryResponse.Answers)
+            {
+                var domainTxtName = _answer.RecordToString();
+                var response = domainTxtName.Substring(1, domainTxtName.Length - 2).Replace(@"\/", "/");
+                txtName = Base64Decode(response);
+            }
 
             //Return normal view when GET is called.
             if (method == "GET")
@@ -15,14 +32,24 @@ namespace StatelessHosting.Controllers
 
             //Populate holding object
 
-
             var holdingPage = new HoldingDetails();
             holdingPage.DomainName = "";
 
-            var holdingThisTest = Request.Body; 
-            
+            var holdingThisTest = Request.Body;
+
             //Just return a simple response write.
             return null;
+        }
+
+        /// <summary>
+        /// Decode a Base64 String 
+        /// </summary>
+        /// <param name="base64EncodedData">Base64 encoded string</param>
+        /// <returns>Plain Text String</returns>
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
     }
 }
